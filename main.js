53086808cd8df34b549b053dc2ab81f92a50bab6
Maggie Leavell <mjleavell@gmail.com>
2018-09-19 20:08:59 -0500

    added array of objects for sorting hat

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+// VARIABLES
+const sortingHat = [
+    {
+        houseName: "gryffindor",
+        houseColor: "scarlet",
+        houseText: "gold"
+    },
+    {
+        houseName: "hufflepuff",
+        houseColor: "yellow",
+        houseText: "black"
+    },
+    {
+        houseName: "ravenclaw",
+        houseColor: "blue",
+        houseText: "bronze"
+    },
+    {
+        houseName: "slytherin",
+        houseColor: "green",
+        houseText: "silver"
+    }
+];
+
 const printToDom = (stringToPrint, elementId) => {
     const selectedDiv = document.getElementById(elementId);
     selectedDiv.innerHTML = stringToPrint;
